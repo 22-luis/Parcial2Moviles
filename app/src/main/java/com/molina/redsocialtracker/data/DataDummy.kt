f3f99803bd5178104d6be8d259950d155583010c
22-luis <00016720@uca.edu.sv>
2023-05-22 13:58:13 -0600

    model

---
+++ app/src/main/java/com/molina/redsocialtracker/data/DataDummy.kt
@@ -0,0 +1,14 @@
+package com.molina.redsocialtracker.data
+
+import com.molina.redsocialtracker.data.model.RedSocialModel
+
+const val name = "Luis"
+const val user = "22-luis"
+
+const val name2 = "Kevin"
+const val user2 = "K3v1n"
+
+var sociales = mutableListOf(
+    RedSocialModel(name,user),
+    RedSocialModel(name2,user2),
+)
---
+++ app/src/main/java/com/molina/redsocialtracker/data/model/RedSocialModel.kt
@@ -0,0 +1,6 @@
+package com.molina.redsocialtracker.data.model
+
+data class RedSocialModel (
+    val name : String,
+    val user : String,
+)
